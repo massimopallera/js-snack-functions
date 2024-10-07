8fc7a7d36069bf819ac19590d7707e6da22ab874
massimopallera <pallera.massimo@gmail.com>
2024-10-07 14:56:22 +0200

    04 done

--- 04 - saluti/script.js
+++ 04 - saluti/script.js
@@ -1,12 +1,13 @@
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
-const name = 'Mario';
+const myName = 'Mario';
 
 
 // Dichiara la funzione qui.
-
+const saluto = str => `Ciao ${str}`
 
 // Invoca la funzione qui e stampa il risultato in console
+console.log(saluto(myName));
 
 
 
