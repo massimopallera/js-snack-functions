1bcd719e1549d847ebfedce42cb66f5342b52830
massimopallera <pallera.massimo@gmail.com>
2024-10-07 14:39:08 +0200

    initial commit + 01 done

---
+++ 01 - Iniziali/script.js
@@ -0,0 +1,24 @@
+/* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
+
+const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
+
+
+// Dichiara la funzione qui.
+
+/**
+ * 
+ * @param {array} array 
+ */
+function takeFirst(array){
+    let newArray = []
+    for (let i = 0; i < array.length; i++){
+        newArray.push(array[i].charAt(0))
+    }
+    return newArray
+}
+
+// Invoca la funzione qui e stampa il risultato in console
+const firstChars = takeFirst(names)
+console.log(firstChars)
+
+//Risultato atteso: ["A", "L", "M", "A", "G", "A"]
