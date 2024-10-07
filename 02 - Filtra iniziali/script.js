3ff2149af4719cf8f6d218cf781ccb6c2dba4e6e
massimopallera <pallera.massimo@gmail.com>
2024-10-07 14:45:02 +0200

    02 done

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -4,9 +4,21 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+// const userInput = 'A'
+const userInput = prompt('Inserisci carattere:')
 
+const namesStartWithChar = (char, array) => {
+    let new_array = []
+    for (let i = 0; i < array.length; i++){
+        if (array[i].charAt(0) === char){
+            new_array.push(array[i])
+        }
+    }
+    return new_array
+}
 
 // Invoca la funzione qui e stampa il risultato in console
+console.log(namesStartWithChar(userInput, names));
 
 
 
