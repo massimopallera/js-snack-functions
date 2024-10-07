7c06f2475ac96229df4dd052296ab7d6c80e21ed
massimopallera <pallera.massimo@gmail.com>
2024-10-07 14:51:48 +0200

    03 done

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -4,9 +4,24 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+function countVocals(str){
+    const vocals = ['a','e','i','o','u']
+    let count = 0
 
+    for (let i =0; i < str.length; i++){
+        for (let j = 0; j < vocals.length; j++){
+            if (str[i] === vocals[j]){
+                count++;
+            }
+        }
+    }
+
+    return count
+}
 
 // Invoca la funzione qui e stampa il risultato in console
+const count = countVocals(word)
+console.log(count);
 
 
 
